0b6dac1740d481bb79e4cb1a7a90f148b8130dc2
John Tucker <john@larkintuckerllc.com>
2020-09-20 06:10:29 -0400

    wild card

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.send('Hello World!');
 });
 
